a98bcb146ea8b7bc3233ea7582e91ad794324028
Nathan Smith <nathan@chainguard.dev>
2022-07-15 11:28:08 -0700

    Format variables.tf

--- variables.tf
+++ variables.tf
@@ -18,13 +18,13 @@ variable "enforce_domain_name" {
 }
 
 variable "enforce_group_id" {
-  type = string
+  type        = string
   description = "Enforce IAM group ID to bind your AWS account to"
-  sensitive = false
-  nullable = false
+  sensitive   = false
+  nullable    = false
 
   validation {
-    condition = regexall("[a-f0-9]{40}(\\/[a-f0-9]{16})*", var.enforce_group_id)
+    condition     = regexall("[a-f0-9]{40}(\\/[a-f0-9]{16})*", var.enforce_group_id)
     error_message = "enforce_group_id must be a valid group id"
   }
 }
