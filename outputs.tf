23214c8b121302367af0ed6040505f6b0461b200
Kenny Leung <kleung@chainguard.dev>
2022-12-29 11:36:48 -0800

    fmt
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "agentless_role_arn" {
-  value = aws_iam_role.agentless_role.arn
+  value       = aws_iam_role.agentless_role.arn
   description = <<-EOF
     This defines a role without permissions in IAM, but which should be authorized
     to manage clusters via:
