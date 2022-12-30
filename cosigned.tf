0d4934e429d1c93d15b5e5a2556bbff88c1e512f
Kenny Leung <kleung@chainguard.dev>
2022-12-30 11:06:58 -0800

    fix c/p error, should be cosigned_role
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- cosigned.tf
+++ cosigned.tf
@@ -26,12 +26,12 @@ resource "aws_iam_role" "cosigned_role" {
 
 // The permissions to grant the "cosigned" role.
 resource "aws_iam_role_policy_attachment" "cosigned_ecr_read" {
-  role       = aws_iam_role.agentless_role.name
+  role       = aws_iam_role.cosigned_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
 resource "aws_iam_role_policy_attachment" "cosigned_kms_pki_read" {
-  role = aws_iam_role.agentless_role.name
+  role = aws_iam_role.cosigned_role.name
   // TODO: Is there a better policy for what we need?
   policy_arn = "arn:aws:iam::aws:policy/AWSKeyManagementServicePowerUser"
 }
