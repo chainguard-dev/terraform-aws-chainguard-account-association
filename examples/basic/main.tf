e0409f5e9b7ab15a5f04a8064419cd40445ee956
Matt Moore <mattmoor@chainguard.dev>
2024-06-09 14:57:16 -0700

    Hollow out the AWS impersonation logic.
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,40 +1,27 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
     chainguard = {
-      # NB: This provider is currently not public
-      source = "chainguard/chainguard"
+      source = "chainguard-dev/chainguard"
     }
   }
 }
-
-provider "chainguard" {
-  console_api = "https://console-api.enforce.dev"
-}
-
-provider "aws" {}
 
 resource "chainguard_group" "root" {
   name        = "demo root"
   description = "root group for demo"
 }
 
+data "aws_caller_identity" "current" {}
+
 module "aws-impersonation" {
   source = "./../../"
 
-  enforce_domain_name = "enforce.dev"
-  enforce_group_id    = chainguard_group.root.id
+  account   = data.aws_caller_identity.current.account_id
+  group_ids = [chainguard_group.root.id]
 }
-
-module "aws-auditlogs" {
-  source = "./../../auditlogs"
-}
-
-data "aws_caller_identity" "current" {}
 
 resource "chainguard_account_associations" "demo-chainguard-dev-binding" {
+  name  = "example"
   group = chainguard_group.root.id
   amazon {
     account = data.aws_caller_identity.current.account_id
